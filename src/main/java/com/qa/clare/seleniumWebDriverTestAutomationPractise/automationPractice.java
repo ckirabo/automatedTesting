faeb2eaf32d22c9d03deefed162b6632978c64e1
clare <kirabo646@googlemail.com>
2019-01-24 17:14:32 +0000

    first commit

--- src/main/java/com/qa/clare/seleniumWebDriverTestAutomationPractise/automationPractice.java
+++ src/main/java/com/qa/clare/seleniumWebDriverTestAutomationPractise/automationPractice.java
@@ -1,7 +1,10 @@
 package com.qa.clare.seleniumWebDriverTestAutomationPractise;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -25,7 +28,8 @@ public class automationPractice {
 	}
 	
 	@Test
-	public void selectDress() throws InterruptedException {
+	
+	public void selectDressToBasket() throws InterruptedException {
 		driver.get(url);
 		
 		WebElement searchBar = driver.findElement(By.xpath("//*[@id=\"search_query_top\"]"));
@@ -39,13 +43,46 @@ public class automationPractice {
 		WebElement checkOut = driver.findElement(By.xpath("//*[@id=\"add_to_cart\"]/button/span"));
 		checkOut.click();
 		
-		/*
-		WebElement continueShopping = driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/span/span"));
-		continueShopping.click();
+		boolean proceedToCheckOutDisplayed = driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a/span"))
+				.isDisplayed();
 		
-		*/
+		boolean proceedToCheckOutEnables = driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a/span"))
+				.isEnabled();
+		
+		if(proceedToCheckOutDisplayed == true &  proceedToCheckOutEnables == true) {
+			WebElement proceed = driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a/span"));
+			proceed.click();
+		}
+		
 		Thread.sleep(8000);
 	}
+	
+	
+	@Test
+	@Ignore
+	public void checkDress() throws InterruptedException {
+        driver.get(url);
+		
+		WebElement searchBar = driver.findElement(By.xpath("//*[@id=\"search_query_top\"]"));
+		searchBar.click();
+		searchBar.sendKeys("dress");
+		searchBar.sendKeys(Keys.ENTER);
+		
+		WebElement dressText = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/h5/a"));
+		dressText.click();
+	
+		WebElement selectedDress = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/div/div[3]/h1"));
+		
+		Thread.sleep(8000);
+		
+		assertEquals("dress not selected","Printed Summer Dress",selectedDress.getText());
+		
+
+		
+	}
+	
+	
+
 	
 	@After
 	public void tearDown() {
